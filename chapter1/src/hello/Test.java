9512e369aa57e03f112993a217e13bf1325ab485
seungAseo <ehdjqthappyskeat9780@naver.com>
2022-05-24 18:04:06 +0900

    chapter1

---
+++ chapter1/src/hello/Test.java
@@ -0,0 +1,21 @@
+package hello;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요");
+	}
+	/*
+	 Java SE 11.0.15 (LTS)
+	 11 - 주 버전: 자바 언어에 많은 변화가 있을 경우 증가됨
+	 0 - 개선 버전: 0부터 시작하고 주 버전에서 일부 사항이 개선될 때 증가됨.
+	 			  보통은 모두 0
+	 15 - 업데이트 버전: 1~3개월 주기로 버그가 수정될 때마당 증가함
+	 LTS - 장기 지원 서비스(Long Term Support)를 받을 수 있는 버전을 말함
+	 
+	 Java SE 11 - 주 버전이 11
+	 Java SE 11.0.15 (LTS) - 주 버전이 11이고, 수정이 15번 되었음,
+	 						 장기 지원 서비스를 제공받을 수 있는 버전
+	 Java SE 8u202 - 주 버전이 8이고, 수정이 202번 되었음.
+	 */
+}
